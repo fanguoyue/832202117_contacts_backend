b9c11020ce485d35db620a13f24a90650da3411d
Chen Zhanming <761199141@qq.com>
2024-10-13 15:07:26 +0800

    832202117_contacts_backend

---
+++ src/java/me/demo/demo1/AddressBookApplication.java
@@ -0,0 +1,11 @@
+package me.demo.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookApplication.class, args);
+    }
+}
---
+++ src/java/me/demo/demo1/WebConfig.java
@@ -0,0 +1,14 @@
+package me.demo.demo1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/contacts").allowedOrigins("http://8.136.126.246:8080"); 
+    }
+}
